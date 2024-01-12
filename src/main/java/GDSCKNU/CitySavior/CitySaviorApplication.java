2f8e92b15373449df61f040067c7c88a89251561
sami355-24 <ddung355@gmail.com>
2024-01-12 12:08:16 +0900

    초기환경 설정

---
+++ src/main/java/GDSCKNU/CitySavior/CitySaviorApplication.java
@@ -0,0 +1,13 @@
+package GDSCKNU.CitySavior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitySaviorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitySaviorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GDSCKNU/CitySavior/CitySaviorApplicationTests.java
@@ -0,0 +1,13 @@
+package GDSCKNU.CitySavior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitySaviorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
