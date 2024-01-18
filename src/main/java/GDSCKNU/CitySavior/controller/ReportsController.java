ed29bc1ab0380b4221e9614e3db2b7c7f46463a6
sami355-24 <ddung355@gmail.com>
2024-01-18 17:34:57 +0900

    Feat: '신고하기'컨트롤러 레이어 구현

---
+++ src/main/java/GDSCKNU/CitySavior/controller/ReportsController.java
@@ -0,0 +1,22 @@
+package GDSCKNU.CitySavior.controller;
+
+import GDSCKNU.CitySavior.dto.ReportRequestDto;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@Slf4j
+public class ReportsController {
+
+    @PostMapping("/reports")
+    public int reports(@RequestPart List<MultipartFile> imgFiles,
+                       @RequestPart ReportRequestDto requestDto) {
+
+        //TODO: 2024-01-18 신고 아이디 값 반환하도록 수정
+        return 0;
+    }
+}
