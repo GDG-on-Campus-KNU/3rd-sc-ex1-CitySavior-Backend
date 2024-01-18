07671b647ecea74b087f68b87646eab8810031ad
sami355-24 <ddung355@gmail.com>
2024-01-18 21:54:22 +0900

    Refactor: 사용하지 않는 Dto 속성 및 import문 제거

--- src/main/java/GDSCKNU/CitySavior/dto/ReportRequestDto.java
+++ src/main/java/GDSCKNU/CitySavior/dto/ReportRequestDto.java
@@ -1,17 +1,14 @@
 package GDSCKNU.CitySavior.dto;
 
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReportRequestDto {
 
-    private List<MultipartFile> imgFiles;
     private double latitude;
     private double longitude;
     private String description;
