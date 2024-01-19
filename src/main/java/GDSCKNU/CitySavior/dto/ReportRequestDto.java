58b64d15196340734c685c2bf2743613bf520157
sami355-24 <ddung355@gmail.com>
2024-01-19 21:41:11 +0900

    Refactor: Dto Record로 변경

--- src/main/java/GDSCKNU/CitySavior/dto/ReportRequestDto.java
+++ src/main/java/GDSCKNU/CitySavior/dto/ReportRequestDto.java
@@ -1,17 +1,4 @@
 package GDSCKNU.CitySavior.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class ReportRequestDto {
-
-    private double latitude;
-    private double longitude;
-    private String description;
-    private int categoryId;
-    private int damageRatio;
+public record ReportRequestDto(double latitude, double longitude, String description, int categoryId, int damageRatio) {
 }
