b8ba72557c8310d92f4070d878b6b8d631ae7840
sami355-24 <ddung355@gmail.com>
2024-01-19 20:13:00 +0900

    Feat: 엔티티간 연관관계 지연로딩으로 변경

--- src/main/java/GDSCKNU/CitySavior/entity/Report.java
+++ src/main/java/GDSCKNU/CitySavior/entity/Report.java
@@ -2,6 +2,7 @@ package GDSCKNU.CitySavior.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -19,7 +20,7 @@ public class Report {
     @Id
     @Column(name = "report_id")
     private Long report_id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
     private double weight;
--- src/main/java/GDSCKNU/CitySavior/entity/ReportComment.java
+++ src/main/java/GDSCKNU/CitySavior/entity/ReportComment.java
@@ -1,6 +1,7 @@
 package GDSCKNU.CitySavior.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -17,7 +18,7 @@ public class ReportComment {
 
     @Id
     private Long report_comment_id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "report_id")
     private Report report;
     private String content;
