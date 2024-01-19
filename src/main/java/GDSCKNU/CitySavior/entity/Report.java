aac90c977c90dff68070956aa1a08d7c5c304c51
sami355-24 <ddung355@gmail.com>
2024-01-19 17:06:27 +0900

    Feat: 엔티티 추가

--- src/main/java/GDSCKNU/CitySavior/entity/Report.java
+++ src/main/java/GDSCKNU/CitySavior/entity/Report.java
@@ -1,4 +1,34 @@
 package GDSCKNU.CitySavior.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Report {
+
+    @Id
+    @Column(name = "report_id")
+    private Long report_id;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+    private double weight;
+    private double latitude;
+    private double longitude;
+    private String description;
+    private String img_url;
+    private double damage_ratio;
+    private LocalDate repaired_date;
+    private LocalDate report_date;
+
 }
---
+++ src/main/java/GDSCKNU/CitySavior/entity/ReportComment.java
@@ -0,0 +1,25 @@
+package GDSCKNU.CitySavior.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReportComment {
+
+    @Id
+    private Long report_comment_id;
+    @ManyToOne
+    @JoinColumn(name = "report_id")
+    private Report report;
+    private String content;
+    private LocalDate create_date;
+}
