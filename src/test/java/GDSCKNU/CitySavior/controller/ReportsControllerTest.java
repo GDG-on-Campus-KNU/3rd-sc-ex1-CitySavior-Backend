e42635478b1ae81988427e4d0b8a7d748771eb3f
sami355-24 <ddung355@gmail.com>
2024-01-22 18:10:10 +0900

    Test: Dto '신고하기'컨트롤러 테스트 코드 추가

--- src/test/java/GDSCKNU/CitySavior/controller/ReportsControllerTest.java
+++ src/test/java/GDSCKNU/CitySavior/controller/ReportsControllerTest.java
@@ -1,7 +1,6 @@
 package GDSCKNU.CitySavior.controller;
 
 
-import static org.aspectj.weaver.tools.cache.SimpleCacheFactory.path;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,7 +15,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -31,22 +29,24 @@ class ReportsControllerTest {
     @DisplayName("파일과 함께 신고에 대한 정보를 받아들였을때 신고가 성공적으로 이루어지는지 확인")
     void 신고하기_정상시나리오() throws Exception {
         //given
-        MockMultipartFile file = new MockMultipartFile(
+        ReportRequestDto testDescription = new ReportRequestDto(37.123456, 127.123456, "testDescription", 1, 1);
+
+        MockMultipartFile imgFiles = new MockMultipartFile(
                 "imgFiles",
                 "testImage.png",
                 MediaType.IMAGE_PNG_VALUE,
                 new FileInputStream("src/test/resources/testImage/testImage.png"));
 
-        ReportRequestDto testDescription = new ReportRequestDto(37.123456, 127.123456, "testDescription", 1, 1);
+        MockMultipartFile requestDto = new MockMultipartFile("requestDto", "", "application/json",
+                new ObjectMapper().writeValueAsString(testDescription).getBytes());
 
         //when
         MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 
         //then
         mockMvc.perform(multipart("/reports")
-                        .file(file)
-                        .content(new ObjectMapper().writeValueAsString(testDescription))
-                        .contentType(MediaType.APPLICATION_JSON))
+                        .file(imgFiles)
+                        .file(requestDto))
                 .andExpect(status().isOk());
     }
 
