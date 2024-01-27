9529315b27c2961ce1142a50f1cb4780909ab6dd
sami355-24 <ddung355@gmail.com>
2024-01-27 20:04:17 +0900

    Feat : 네이티브 쿼리로 반경검색을 하는 레포지토리 메서드 작성

--- src/main/java/GDSCKNU/CitySavior/repository/ReportRepository.java
+++ src/main/java/GDSCKNU/CitySavior/repository/ReportRepository.java
@@ -1,13 +1,21 @@
 package GDSCKNU.CitySavior.repository;
 
 import GDSCKNU.CitySavior.entity.Report;
+import java.util.List;
 import java.util.Optional;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
     Optional<Report> findById(Long reportId);
+
+    @Query(value = "SELECT * FROM Report r " +
+            "WHERE ST_DWithin(r.location, :point, :radius, false) = true", nativeQuery = true)
+    List<Report> findReportsWithinRadius(@Param("point") Point point, @Param("radius") double radius);
 
 }
---
+++ src/test/java/GDSCKNU/CitySavior/repository/ReportRepositoryTest.java
@@ -0,0 +1,63 @@
+package GDSCKNU.CitySavior.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import GDSCKNU.CitySavior.entity.Report;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportRepositoryTest {
+
+    @Autowired
+    private ReportRepository reportRepository;
+
+    @Autowired
+    private GeometryFactory geometryFactory;
+
+    @Test
+    @DisplayName("인자로 주어지는 반경 안에 값이 존재하면 값을 반환한다.")
+    void findReportsWithinRadiusTest() {
+        //given
+        Report dummyReport1 = Report.builder()
+                .weight(1.0)
+                .location(geometryFactory.createPoint(new Coordinate(1.0, 1.0)))
+                .description("test")
+                .img_url("test")
+                .damage_ratio(1.0)
+                .repaired_date(null)
+                .report_date(LocalDate.now())
+                .comments(new ArrayList<>())
+                .build();
+
+        Report dummyReport2 = Report.builder()
+                .weight(1.0)
+                .location(geometryFactory.createPoint(new Coordinate(2.0, 2.0)))
+                .description("test")
+                .img_url("test")
+                .damage_ratio(1.0)
+                .repaired_date(null)
+                .report_date(LocalDate.now())
+                .comments(new ArrayList<>())
+                .build();
+
+        Point point = geometryFactory.createPoint(new Coordinate(1.0, 1.0));
+
+        //when
+        reportRepository.saveAll(List.of(dummyReport1, dummyReport2));
+        System.out.printf("dummyReport1: %s\n", dummyReport1.getReport_id());
+
+        List<Report> reports = reportRepository.findReportsWithinRadius(point, 1000.0);
+
+        //then
+        assertEquals(1, reports.size());
+    }
+}
