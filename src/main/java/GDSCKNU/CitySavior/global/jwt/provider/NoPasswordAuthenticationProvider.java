17eaaac6c911fef38bda40c9f2fe8242ea15df2e
jinchiim <nellroll@syuin.ac.kr>
2024-01-31 00:16:51 +0900

    feat(NoPasswordAuthenticationProvider.java): Provider 구현 - 비밀번호 없이 인가를 받도록 Provider 상속 후 구현 - Custom Token 생성

---
+++ src/main/java/GDSCKNU/CitySavior/global/jwt/provider/NoPasswordAuthenticationProvider.java
@@ -0,0 +1,29 @@
+package GDSCKNU.CitySavior.global.jwt.provider;
+
+import GDSCKNU.CitySavior.global.jwt.entity.NoPasswordAuthenticationToken;
+import GDSCKNU.CitySavior.service.member.detail.MemberDetailServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class NoPasswordAuthenticationProvider implements AuthenticationProvider {
+
+    private final MemberDetailServiceImpl memberDetailService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        UserDetails userDetails = memberDetailService.loadUserByUsername(authentication.getName());
+        // 원래는.. 여러 검사를 해야하지만 비밀번호가 없는 관계로 패스
+        return authentication;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return NoPasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
