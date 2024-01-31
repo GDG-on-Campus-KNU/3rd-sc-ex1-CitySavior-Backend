c4aa80fc5b7f9286f482fcbc9ffa014a75d31b50
jinchiim <nellroll@syuin.ac.kr>
2024-01-31 12:44:52 +0900

    fix(NoPasswordAuthenticationProvider.java): Authentication Provider 수정 - 파라미터 정확히 명시 - Authentication 생성 후 전달하도록 수정

--- src/main/java/GDSCKNU/CitySavior/global/jwt/provider/NoPasswordAuthenticationProvider.java
+++ src/main/java/GDSCKNU/CitySavior/global/jwt/provider/NoPasswordAuthenticationProvider.java
@@ -1,12 +1,12 @@
 package GDSCKNU.CitySavior.global.jwt.provider;
 
+import GDSCKNU.CitySavior.entity.memberDetail.MemberDetailsImpl;
 import GDSCKNU.CitySavior.global.jwt.entity.NoPasswordAuthenticationToken;
 import GDSCKNU.CitySavior.service.member.detail.MemberDetailServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,9 +17,9 @@ public class NoPasswordAuthenticationProvider implements AuthenticationProvider 
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        UserDetails userDetails = memberDetailService.loadUserByUsername(authentication.getName());
+        MemberDetailsImpl userDetails = memberDetailService.loadUserByUsername(authentication.getName());
         // 원래는.. 여러 검사를 해야하지만 비밀번호가 없는 관계로 패스
-        return authentication;
+        return new NoPasswordAuthenticationToken(userDetails.getUsername(), null, userDetails.getAuthorities());
     }
 
     @Override
