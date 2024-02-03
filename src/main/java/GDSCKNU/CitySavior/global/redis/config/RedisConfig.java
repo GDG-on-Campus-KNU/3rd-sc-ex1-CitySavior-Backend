6c29a1c3def6861c66aedf699eb947d5b0ec9e4f
jinchiim <nellroll@syuin.ac.kr>
2024-02-03 12:54:51 +0900

    fix(RedisConfig.java): 비밀번호 설정 추가 - Redis 비밀번호 추가

--- src/main/java/GDSCKNU/CitySavior/global/redis/config/RedisConfig.java
+++ src/main/java/GDSCKNU/CitySavior/global/redis/config/RedisConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -20,9 +21,16 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
+        redisConfiguration.setPort(redisPort);
+        redisConfiguration.setHostName(redisHost);
+        redisConfiguration.setPassword(password);
+        return new LettuceConnectionFactory(redisConfiguration);
     }
 
     @Bean
