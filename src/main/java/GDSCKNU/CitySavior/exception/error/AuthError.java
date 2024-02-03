9dce2b5a96a53b625355ee50008fb7f1df4c67d6
jinchiim <nellroll@syuin.ac.kr>
2024-02-03 12:54:29 +0900

    feat(AuthError.java): AuthError  추가 - AuthError 추가

--- src/main/java/GDSCKNU/CitySavior/exception/error/AuthError.java
+++ src/main/java/GDSCKNU/CitySavior/exception/error/AuthError.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum AuthError implements ErrorEnumBase {
 
-    TOKEN_NOT_FOUND_ERROR(HttpStatus.UNAUTHORIZED, "주어진 토큰이 Null 이거나 존재하지 않습니다.");
+    TOKEN_NOT_FOUND_ERROR(HttpStatus.UNAUTHORIZED, "주어진 토큰이 Null 이거나 존재하지 않습니다."),
+
+    TOKEN_REISSUE_FAILED_ERROR(HttpStatus.UNAUTHORIZED, "reissue에 실패했습니다. Token이 null 입니다.");
 
     final HttpStatus status;
     final String message;
