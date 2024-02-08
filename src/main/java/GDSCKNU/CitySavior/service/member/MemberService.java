23cec9293e6a90eabbf2d278dad0104b74625f2a
jinchiim <nellroll@syuin.ac.kr>
2024-02-08 22:29:01 +0900

    feat(MemberService.java): transactional move

--- src/main/java/GDSCKNU/CitySavior/service/member/MemberService.java
+++ src/main/java/GDSCKNU/CitySavior/service/member/MemberService.java
@@ -7,6 +7,7 @@ import GDSCKNU.CitySavior.exception.error.MemberError;
 import GDSCKNU.CitySavior.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +15,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional
     public void registerMember(MemberCreateV1Request createV1Request) {
         Boolean isExistMember = memberRepository.existsByEmail(createV1Request.email());
 
