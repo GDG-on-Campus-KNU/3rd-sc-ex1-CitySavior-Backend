a8866ff6c3593295e0043bf64ca0a56e0100fccf
sami355-24 <ddung355@gmail.com>
2024-02-12 16:52:01 +0900

    Feat: 신고 관련 로직 수행중 예외 발생시 서비스에서 사용자 정의 예외를 던지도록 코드 수정

--- src/main/java/GDSCKNU/CitySavior/exception/error/ReportError.java
+++ src/main/java/GDSCKNU/CitySavior/exception/error/ReportError.java
@@ -8,8 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ReportError implements ErrorEnumBase {
 
-    REPORT_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "존재하는 신고을 찾지 못했습니다."),
-    AI_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "AI 서버와 통신 중 오류가 발생했습니다.");
+    REPORT_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "존재하는 신고를 찾지 못했습니다.");
 
     final HttpStatus status;
     final String message;
