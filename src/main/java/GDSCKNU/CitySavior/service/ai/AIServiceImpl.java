412c1cce72260698ee76554b4847892ab6e69e3a
sami355-24 <ddung355@gmail.com>
2024-02-19 17:22:25 +0900

    Fix: AI 서버에 요청 보낼시 예외가 발생하는 로직 수정

--- src/main/java/GDSCKNU/CitySavior/service/ai/AIServiceImpl.java
+++ src/main/java/GDSCKNU/CitySavior/service/ai/AIServiceImpl.java
@@ -2,11 +2,16 @@ package GDSCKNU.CitySavior.service.ai;
 
 import GDSCKNU.CitySavior.exception.AIException;
 import GDSCKNU.CitySavior.exception.error.AIError;
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
@@ -28,7 +33,7 @@ public class AIServiceImpl implements AIService {
 
         webClient.post()
                 .uri(aiServerUri + aiServerPath)
-                .bodyValue(imgFiles)
+                .body(BodyInserters.fromMultipartData(makeFormData(imgFiles)))
                 .retrieve()
                 .bodyToMono(Integer.class)
                 .subscribe(
@@ -39,4 +44,20 @@ public class AIServiceImpl implements AIService {
 
         return weight.get();
     }
+
+    private MultiValueMap<String, Object> makeFormData(MultipartFile imgFiles) {
+        MultiValueMap<String, Object> formData = new LinkedMultiValueMap<>();
+        try{
+            formData.add("image", new ByteArrayResource(imgFiles.getBytes()) {
+                @Override
+                public String getFilename() {
+                    return "image.jpg";
+                }
+            });
+        } catch (IOException e) {
+            throw new AIException(AIError.AI_SERVER_ERROR);
+        }
+        return formData;
+    }
+
 }
